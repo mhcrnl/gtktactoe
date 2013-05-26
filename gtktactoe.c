3375bca3de7c934d9e14af549dd528b5c0a9891a
dejay <chipsolderer@gmail.com>
2013-05-25 19:17:16 -0600

    fixed tabbing in gtktactoe.c

--- gtktactoe.c
+++ gtktactoe.c
@@ -137,61 +137,61 @@ int main(int argc, char **argv) {
 		/* Build GTK Layout */
 		if(VERBOSE) printf("Building Interface...");
 
-			/* Main Window */
-			window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-			g_signal_connect(window, "delete-event", G_CALLBACK(finish), 0);
+		/* Main Window */
+		window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+		g_signal_connect(window, "delete-event", G_CALLBACK(finish), 0);
 
-			/* VBox */
-			vBox = gtk_vbox_new(FALSE, 0);
-			gtk_container_add(GTK_CONTAINER(window), vBox);
+		/* VBox */
+		vBox = gtk_vbox_new(FALSE, 0);
+		gtk_container_add(GTK_CONTAINER(window), vBox);
 
-			/* Menubar */
-			menubar = gtk_menu_bar_new();
-			filemenu = gtk_menu_new();
+		/* Menubar */
+		menubar = gtk_menu_bar_new();
+		filemenu = gtk_menu_new();
 
-			accel_group = gtk_accel_group_new();
-			gtk_window_add_accel_group(GTK_WINDOW(window), accel_group);
+		accel_group = gtk_accel_group_new();
+		gtk_window_add_accel_group(GTK_WINDOW(window), accel_group);
 
-			file = gtk_menu_item_new_with_mnemonic("_Game");
-			newGameButton = gtk_image_menu_item_new_from_stock(GTK_STOCK_NEW, accel_group);
-			checkbox = gtk_check_menu_item_new_with_label("Computer Game");
-			quitButton = gtk_image_menu_item_new_from_stock(GTK_STOCK_QUIT, accel_group);
+		file = gtk_menu_item_new_with_mnemonic("_Game");
+		newGameButton = gtk_image_menu_item_new_from_stock(GTK_STOCK_NEW, accel_group);
+		checkbox = gtk_check_menu_item_new_with_label("Computer Game");
+		quitButton = gtk_image_menu_item_new_from_stock(GTK_STOCK_QUIT, accel_group);
 
-			gtk_menu_item_set_submenu(GTK_MENU_ITEM(file), filemenu);
-			gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), newGameButton);
-			gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), checkbox);
-			gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), gtk_separator_menu_item_new());
-			gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), quitButton);
-			gtk_menu_shell_append(GTK_MENU_SHELL(menubar), file);
-			gtk_box_pack_start(GTK_BOX(vBox), menubar, FALSE, FALSE, 0);
+		gtk_menu_item_set_submenu(GTK_MENU_ITEM(file), filemenu);
+		gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), newGameButton);
+		gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), checkbox);
+		gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), gtk_separator_menu_item_new());
+		gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), quitButton);
+		gtk_menu_shell_append(GTK_MENU_SHELL(menubar), file);
+		gtk_box_pack_start(GTK_BOX(vBox), menubar, FALSE, FALSE, 0);
 
-			g_signal_connect(newGameButton, "activate", G_CALLBACK(newGameEvent), 0);
-			g_signal_connect(checkbox, "activate", G_CALLBACK(checkboxEvent), 0);
-			g_signal_connect(quitButton, "activate", G_CALLBACK(finish), 0);
+		g_signal_connect(newGameButton, "activate", G_CALLBACK(newGameEvent), 0);
+		g_signal_connect(checkbox, "activate", G_CALLBACK(checkboxEvent), 0);
+		g_signal_connect(quitButton, "activate", G_CALLBACK(finish), 0);
 
-			/* TicTacToe Grid */
-			board = gtk_grid_new();
-			gtk_grid_set_row_homogeneous(board, gtk_true());
-			gtk_grid_set_column_homogeneous(board, gtk_true());
-			gtk_container_add(GTK_CONTAINER(vBox), board);
+		/* TicTacToe Grid */
+		board = gtk_grid_new();
+		gtk_grid_set_row_homogeneous(board, gtk_true());
+		gtk_grid_set_column_homogeneous(board, gtk_true());
+		gtk_container_add(GTK_CONTAINER(vBox), board);
 
-			/* TicTacToe Turn Indicator */
-			label = gtk_label_new(NULL);
-			gtk_grid_attach(board, label, 0, 0, 3, 1);
+		/* TicTacToe Turn Indicator */
+		label = gtk_label_new(NULL);
+		gtk_grid_attach(board, label, 0, 0, 3, 1);
 
-			/* TicTacToe Cells */
-			for(i = 0; i < 9; i++) {
-				cells[i].button = gtk_button_new();
-				cells[i].index = i;
-				cells[i].handler = g_signal_connect(cells[i].button, "clicked", G_CALLBACK(clickEvent), &cells[i]);
-				sprintf(labelText, "%s/share/gtktactoe/sprites/empty.png", PATH);
-				gtk_button_set_image(cells[i].button, gtk_image_new_from_file(labelText));
-				gtk_grid_attach(board, cells[i].button, i % 3, (i / 3) + 1, 1, 1);
-			}
+		/* TicTacToe Cells */
+		for(i = 0; i < 9; i++) {
+			cells[i].button = gtk_button_new();
+			cells[i].index = i;
+			cells[i].handler = g_signal_connect(cells[i].button, "clicked", G_CALLBACK(clickEvent), &cells[i]);
+			sprintf(labelText, "%s/share/gtktactoe/sprites/empty.png", PATH);
+			gtk_button_set_image(cells[i].button, gtk_image_new_from_file(labelText));
+			gtk_grid_attach(board, cells[i].button, i % 3, (i / 3) + 1, 1, 1);
+		}
 
-			gtk_widget_show_all(window);
+		gtk_widget_show_all(window);
 
-			if(VERBOSE) printf("DONE\n");
+		if(VERBOSE) printf("DONE\n");
 
 		/* Initialize the game engine */
 		if(VERBOSE) printf("Initializing Engine...");
@@ -294,7 +294,7 @@ int main(int argc, char **argv) {
 				/* Reset the images */
 				for(i = 0; i < 9; i++) {
 					sprintf(labelText, "%s/share/gtktactoe/sprites/empty.png", PATH);
-                                	gtk_button_set_image(cells[i].button, gtk_image_new_from_file(labelText));
+					gtk_button_set_image(cells[i].button, gtk_image_new_from_file(labelText));
 				}
 
 				/* Make the cells sensitive again */
@@ -329,7 +329,7 @@ int main(int argc, char **argv) {
 			/* Reset the images */
 			for(i = 0; i < 9; i++) {
 				sprintf(labelText, "%s/share/gtktactoe/sprites/empty.png", PATH);
-                                gtk_button_set_image(cells[i].button, gtk_image_new_from_file(labelText));
+				gtk_button_set_image(cells[i].button, gtk_image_new_from_file(labelText));
 			}
 
 			/* Make the cells sensitive again */
